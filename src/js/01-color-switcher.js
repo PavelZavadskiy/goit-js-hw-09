6205c6f331ac2cfb4686722915386befaa54ec7d
Pavel <87939143+PavelZavadskiy@users.noreply.github.com>
2023-07-19 21:03:26 +0300

    Added Color switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const start = document.querySelector('[data-start]');
+const stop = document.querySelector('[data-stop]');
+stop.disabled = true;
 
+let timer = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+const reverseDisabled = () => {
+  stop.disabled = !stop.disabled;
+  start.disabled = !start.disabled;
+};
+
+start.addEventListener('click', () => {
+  reverseDisabled();
+  timer = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+});
+
+stop.addEventListener('click', () => {
+  reverseDisabled();
+  clearInterval(timer);
+});
