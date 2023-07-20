f7b8d965627ed4bb78acbcbf45cd8f898c0f10f3
PavelZavadskiy <87939143+PavelZavadskiy@users.noreply.github.com>
2023-07-20 17:25:51 +0000

    Deploying to gh-pages from @ PavelZavadskiy/goit-js-hw-09@f073c49204fe30cf24113cf34a2fc97e57960519 🚀

---
+++ 01-color-switcher.17769edf.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]");t.disabled=!0;let d=null;const a=()=>{t.disabled=!t.disabled,e.disabled=!e.disabled};e.addEventListener("click",(()=>{a(),d=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3)})),t.addEventListener("click",(()=>{a(),clearInterval(d)}));
+//# sourceMappingURL=01-color-switcher.17769edf.js.map
